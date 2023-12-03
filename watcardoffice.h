3974323e3d459ad3c5f13b5f1840cf595784a9eb
Kevin Xu <y633xu@uwaterloo.ca>
2023-12-03 12:34:53 -0500

    add watcardoffice

--- watcardoffice.h
+++ watcardoffice.h
@@ -1,18 +1,39 @@
 #ifndef WATCARDOFFICE_H
 #define WATCARDOFFICE_H
 
+#include <queue>
+
+#include "watcard.h"
+
 _Task WATCardOffice {
 	struct Job {							// marshalled arguments and return future
 		Args args;							// call arguments (YOU DEFINE "Args")
 		WATCard::FWATCard result;			// return future
 		Job( Args args ) : args( args ) {}
 	};
-	_Task Courier { ... };					// communicates with bank
+	_Task Courier { // communicates with bank
+		Printer & prt;
+		Bank & bank;
+		WATCardOffice * office;
+		unsigned int id;
+		void main();
+
+	  public:
+		Courier( Printer & prt, Bank & bank, WATCardOffice * office, unsigned int id );
+
+	};					
+
+	Printer & prt;
+	Bank & bank;
+	unsigned int numCouriers;
+	std::queue<Job> jobs;
+	Courier ** couriers;
 
 	void main();
   public:
 	_Event Lost {};							// lost WATCard
 	WATCardOffice( Printer & prt, Bank & bank, unsigned int numCouriers );
+	~WATCardOffice() {
 	WATCard::FWATCard create( unsigned int sid, unsigned int amount ) __attribute__(( warn_unused_result ));
 	WATCard::FWATCard transfer( unsigned int sid, unsigned int amount, WATCard * card ) __attribute__(( warn_unused_result ));
 	Job * requestWork() __attribute__(( warn_unused_result ));
