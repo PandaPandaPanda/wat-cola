6dc81108f94cf02837112b98897eec9cd98dd60a
Kevin Xu <y633xu@uwaterloo.ca>
2023-12-03 21:52:08 -0500

    complete nameserver

--- nameServer.cc
+++ nameServer.cc
@@ -1,28 +1,63 @@
 #include "nameServer.h"
 #include <iostream>
 
+// ==== Global debug
+#include <iostream>
+extern bool debug;
+// ====
+
 using namespace std;
 
 NameServer::NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents )
     : prt(prt), numVendingMachines(numVendingMachines), numStudents(numStudents) {
         machines = new VendingMachine*[numVendingMachines]();
+        
+        sid_to_machineid = new unsigned int[numStudents];
+        // logically distributing the students evenly across the vending machines in a round-robin fashion
+        for (unsigned int i = 0; i < numStudents; i++) {
+            sid_to_machineid[i] = i % numVendingMachines;
+        }
     };
 
+NameServer::~NameServer() {
+    delete[] machines; // delete array, vending machine itself is cleanup elsewhere
+    delete[] sid_to_machineid;
+}
+
 void NameServer::main() {
+    PRINT( prt.print(Printer::NameServer, 'S'); )
+
     for (;;) {
+        // all machines are registered before accepting calls to other members
         _Accept(~NameServer) {
             break;
-        } or _Accept(VMregister) {
-            cout << "vm register called" << endl;
-        } or _Accept(getMachineList) {
-            cout << "machine list" << endl;
+        } or _When(machine_cnt < numVendingMachines) _Accept(VMregister) {
+            machine_cnt+=1;
+            PRINT( prt.print(Printer::NameServer, 'R', machine_cnt); )
+        } or _When(machine_cnt == numVendingMachines) _Accept(getMachine) { 
+            /*
+            update the student's assigned machine id
+            name server must cycle through the vending machines separately for each student 
+            starting from the initial position via modulo incrementing to ensure a student 
+            has a chance to visit every machine
+            */ 
+            PRINT( prt.print(Printer::NameServer, 'N', cur_id, sid_to_machineid[cur_id]);)
+            sid_to_machineid[cur_id] = (sid_to_machineid[cur_id] + 1) % numVendingMachines;
+        } or _When(machine_cnt == numVendingMachines) _Accept(getMachineList) {
+            if (debug) {cout << "machine list" << endl;}
         } _Else {}
     }
+
+    PRINT( prt.print(Printer::NameServer, 'F'); )
 }
 
 void NameServer::VMregister( VendingMachine * vendingmachine ) {
-    machines[cur_machine] = vendingmachine;
-    cur_machine+=1;
+    machines[machine_cnt] = vendingmachine;
+}
+
+VendingMachine * NameServer::getMachine( unsigned int id ) {
+    cur_id = id; // save value of id for main to use
+    return machines[sid_to_machineid[id]]; // get the machine student is currently assigned to
 }
 
 VendingMachine** NameServer::getMachineList() {
--- nameServer.h
+++ nameServer.h
@@ -1,20 +1,26 @@
 #ifndef NAMESERVER_H
 #define NAMESERVER_H
 
-_Monitor Printer;
-_Task VendingMachine;
+#include "printer.h"
+#include "vendingmachine.h"
 
 _Task NameServer {
 	Printer & prt;
 	unsigned int numVendingMachines;
 	unsigned int numStudents;
+	unsigned int machine_cnt = 0;
 	VendingMachine** machines;
-	unsigned int cur_machine = 0;
+	unsigned int* sid_to_machineid;
+	
+	// buffer variables for storing param variables for task main
+	unsigned int cur_id;
+
 	void main();
   public:
 	NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
+	~NameServer();
 	void VMregister( VendingMachine * vendingmachine );
-	// VendingMachine * getMachine( unsigned int id ) __attribute__(( warn_unused_result ));
+	VendingMachine * getMachine( unsigned int id ) __attribute__(( warn_unused_result ));
 	VendingMachine ** getMachineList() __attribute__(( warn_unused_result ));
 };
 
